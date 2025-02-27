6cf20e27bb79b7c852744f7ed1eb2bcc8c0d3b0a
Harshil Modi <88607540+harshil2003@users.noreply.github.com>
2025-02-27 00:57:50 +0000

    Add files via upload

---
+++ train.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "6d3dac3b-0c15-45db-8077-947e7c5c12f6",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
